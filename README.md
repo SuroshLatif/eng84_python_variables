9a440c40dd942544eb68532c233c8f0f36cc8828
SuroshLatif <80529254+SuroshLatif@users.noreply.github.com>
2021-03-22 17:56:43 +0000

    Pushed .gitignore and README.md and variables.py file to github

---
+++ README.md
@@ -0,0 +1,13 @@
+#Python Variables 
+
+
+## .gitignore to ignore files that we dont want to send to git hub
+
+### .gitignore with python variables and README.md pushed to github
+
+ - To add a comment in .py file we use "#"
+
+
+# What is a variable? 
+
+- Variables work as a place holder
---
+++ variables.py
@@ -0,0 +1,43 @@
+# sudo coding
+
+# Testing the enviroment with hello world message
+#print("Hello World")
+
+# print("Surosh")
+
+# What is a variable?
+# course_name = "devops 84" # String
+# Student_name = "Surosh" # String
+# age = 16 # Integer (Int)
+# hourly_wage = 8.5 # float
+
+# print(course_name)
+# print(hourly_wage)
+# print(age)
+
+# To get user data we use input()
+# first_name = input("Please enter your name")
+# age = input("Please enter your age")
+# print("hello ")
+# print(first_name)
+
+# To check type of data()
+# print(type(age))
+# overwriting a variable?
+
+# first_name = "save"
+# print(first_name)
+
+first_name = input("What is your first name?")
+print(first_name)
+last_name = input("What is your last name")
+print(last_name)
+age = int(input("What is your age?"))
+print(age)
+salary = int(input("What is your salary"))
+print = (salary)
+date_of_birth = input("What is your date of birth")
+print = (date_of_birth)
+post_code = input("What is your Postcode?")
+print = (post_code)
+
